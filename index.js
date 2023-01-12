282737756812ee7f082e5d006817aeec68cdc7e0
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-01-12 22:21:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var readlineSync = require("readline-sync");
+var question = readlineSync.question("What is your favorite  sports? ");
+console.log("Amazing, " + question + "is a royal and luxrious sports");
+
+function play(ques, answ) {
+  var userInfo = readlineSync.question(ques);
+  console.log(userInfo);
+  count = 0;
+  if (userInfo == answ) {
+    console.log("Data given is valid");
+    count = count + 1;
+  } else {
+    console.log("Data given in invalid");
+    count = count - 1;
+  }
+
+}
+
+var quizzBank = [{
+  que: "Who is 2022 f1 wdc? a. Max b. Lewis c. Alonso d. Leclerc ",
+  ans: "Max"
+},
+{
+  que: "Who is 2021 f1 wdc? a. Lewis b. Vettel c. Riccardo d. Max",
+  ans: "Max"
+},
+{
+  que: "Who won constructor championship in 2021 a. Ferrari b. Redbll c.         Mercedes",
+  ans: "Mercedes"
+},
+{
+  que: "Who is the best driver in F1? a. Ayton Sennar b. Schumacher c.           Alonso, d. Max",
+  ans: "Schumacher"
+},
+{
+  que: "How many title ALonso has in f1? a. 2, b. 3, c. 6, d. 7",
+  ans: "2"
+}]
+
+for (var i = 0; i <= quizzBank.length; i++) {
+  var userData = quizzBank[i];
+  play(userData.que, userData.ans);
+  console.log("Current count is: " + count);
+  console.log("----------------");
+}
